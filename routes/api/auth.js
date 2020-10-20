03d30e63a8d8656bf5eba114fdc3f57d27d8abe4
Jeff Knowles Jr <jeffknowlesjr@gmail.com>
2020-10-20 01:43:42 -0400

    Update auth.js

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,11 +2,12 @@
 const express = require('express');
 // Creates variable to use express router
 const router = express.Router();
+const auth = require('../../middleware/auth')
 
 // @route   GET api/auth
 // @desc    Test Route
-// @access  Public
-router.get('/', (req, res) => res.send('Auth route'));
+// @access  Protected
+router.get('/', auth, (req, res) => res.send('Auth route'));
 
 // Exports the router
 module.exports = router;

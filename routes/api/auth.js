802ac7420def1a0e9566a11ea38250f16170f223
Jeff Knowles <jeffknowlesjr@gmail.com>
2020-10-20 18:23:20 -0400

    Finished User Login Route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,8 +2,12 @@
 const express = require('express');
 // Creates variable to use express router
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 const auth = require('../../middleware/auth')
 const User = require('../../models/User');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
 
 // @route   GET api/auth
 // @desc    Test Route
@@ -20,6 +24,82 @@ router.get('/', auth, async (req, res) => {
     console.error(err.message);
     res.status(500).send('Server Error');
   }
+
+});
+
+// @route   POST api/auth
+// @desc    Authenticate user & get token
+// @access  Public
+router.post('/', [
+  check('email', 'Please include a valid email').isEmail(),
+  check('password', 'Password is required').exists()
+],
+async (req, res) => {
+  const errors = validationResult(req);
+  if(!errors.isEmpty()){
+    return res.status(400).json({ errors: errors.array() })
+  }
+
+  // Destructuring Request dot Body
+  const { name, email, password } = req.body;
+
+  try {
+    // See if user exists
+    let user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ errors: [{ msg: 'Invalid Credentials' }] });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({ errors: [{ msg: 'Invalid Credentials' }] });
+    }
+
+    // Return jsonwebtoken
+    // Setting up user login 
+    const payload = {
+      user: {
+        id: user.id
+      }
+    }
+
+    // jetWoken // jwtToken
+    // --------
+    // jwt signs the token
+    // passing in the payload
+    // containing the User ID
+    // also and also passing
+    // in our Secret Key (token)
+    // an expirationIn order
+    // and a call back function
+    // to receive the status
+    // that sends in response
+    // the signed token
+
+
+    jwt.sign(
+      payload,
+      config.get('jwtSecret'),
+      { expiresIn: 360000 },
+      (err, token) => {
+        // console.log('X gon give ya Errors')
+        if (err) throw err;
+        res.json({ token });
+      }
+    );
+
+    // We're going to taken this
+    // and pass it through the headers
+    // in access protected routes
+    
+  } catch(err){
+    console.error(err.message);
+    res.status(500).send('Sever error');
+  }
+
+
 
 });
 
